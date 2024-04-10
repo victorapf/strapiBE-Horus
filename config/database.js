1ee1f1a8ab0a54728caec33b082ae5ca001c053f
Victor Perez <strangervp3@gmail.com>
2024-04-09 23:12:45 -0400

    new fix

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env("DATABASE_HOST","aws-0-us-west-1.pooler.supabase.com"),
+      port: env("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USER", "postgres"),
+      password: env("DATABASE_PASSWORD", "GJLH6oGTM7ByzidS"),
+      ssl: env("DATABASE_SSL", false)
+    },
+    useNullAsDefault: false
+  },
+})
