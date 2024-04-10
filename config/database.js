cd73ad68af43e60c17ab59ae28df4379d0c603ed
Victor Perez <strangervp3@gmail.com>
2024-04-09 22:36:53 -0400

    fix

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({env}) => ({
       host: env("DATABASE_HOST","aws-0-us-west-1.pooler.supabase.com"),
       port: env("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USER", "postgres.epprffxpiyayyoduhpgm"),
+      user: env("DATABASE_USER", "postgres"),
       password: env("DATABASE_PASSWORD", "GJLH6oGTM7ByzidS"),
       ssl: env("DATABASE_SSL", false)
     },
